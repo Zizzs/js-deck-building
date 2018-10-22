d7c0b0b3377189c6fadfdac9c965e1e2c13c4953
Alex Williams and Charles Robertson <charles@>
2018-10-22 16:19:47 -0700

    Made code a bit more streamlined by removing unneeded variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
-suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
-combinedArray = [];
+var cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
+var suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
+var combinedArray = [];
 var cardArrayLength = parseInt(cards.length);
 var suitArrayLength = parseInt(suits.length);
 var combinedArrayLength = parseInt(combinedArray.length);
@@ -9,10 +9,8 @@ $(document).ready(function(event) {
     $("#formInfo").submit(function(event) {
       event.preventDefault();
         suits.forEach(function(suit) {
-          var newSuit = suit;
             cards.forEach(function(card) {
-              var newCard = card;
-              combinedCard = (newCard + " of " + newSuit);
+              combinedCard = (card + " of " + suit);
               combinedArray.push(combinedCard);
               if (combinedArrayLength < 52) {
                 $("#output").append("<li>"+combinedCard);
